2c1050cdefdb067fa19f14a5176b6d152abf1382
AgataM <agata.majer@wp.pl>
2022-09-20 12:19:27 +0200

    The name of javascript

---
+++ The name of javascript/The name of javascript 2.js
@@ -0,0 +1,8 @@
+let officialNameQuestion = prompt('What\'s the \'official\' name of JavaScript?').toString().toUpperCase();
+
+if(officialNameQuestion === 'ECMASCRIPT'){
+    alert('Right!');
+}
+else{
+    alert('You don\'t know? ECMAScript!');
+}
